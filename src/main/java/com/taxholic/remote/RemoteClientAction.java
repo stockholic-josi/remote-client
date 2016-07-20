de14a938fc26c92567f13c13b34e8329e8e7e255
taxholic <parkjjss2@gmail.com>
2016-07-20 12:45:09 +0900

    Update RemoteClientAction.java

--- src/main/java/com/taxholic/remote/RemoteClientAction.java
+++ src/main/java/com/taxholic/remote/RemoteClientAction.java
@@ -44,74 +44,24 @@ public class RemoteClientAction {
 		JSchUtil js = login(key);
 		
 		if(shFile != null){
+			
+			//설치 스크립트 시랭
 			js.scpTo(new File(shFile), "install_"+ server  +".sh");
-			String[] cmd = {
+			String[] installCmd = {
 				"chmod 755 install_" + server  + ".sh"
 				,"./install_" + server  + ".sh"
 			};
-			js.exec(cmd);
+			js.exec(installCmd);
 			
-			setConfig(server, resource, js);
+			//설치 후 작업 server.properties
+			copyResource(server,js);
+			String[] afterCmd =  prop.getStringArray(server + ".cmd");
+			if(afterCmd.length > 0) js.exec(afterCmd);
 			
 		}
 	}
 	
-	public void setConfig(String server, String resource, JSchUtil js){
-		
-		if("apache".equals(server)){
-			String[] frFile =	{resource + "/config/" + server + "/httpd.conf"
-									,resource + "/config/" + server + "/workers.properties"};
-			String[] toFile =	{"/usr/local/"+ server +"/conf/httpd.conf"
-									,"/usr/local/" + server + "/conf/workers.properties"};
-			copyRemote(frFile,toFile,js);
-			
-		}else if("nginx".equals(server)){
-			String[] frFile =	{resource + "/config/" + server + "/nginx.conf"
-									,resource + "/config/" + server + "/nginxd"};
-			String[] toFile =	{"/usr/local/" + server + "/conf/nginx.conf"
-									,"/etc/rc.d/init.d/nginxd"};
-			copyRemote(frFile,toFile,js);
-			 
-			String[] cmd = {
-				"chmod 755 /etc/rc.d/init.d/nginxd"
-			};
-			js.exec(cmd);
-			 
-		}else 	if("tomcat".equals(server)){
-			 copyRemote(server,js);
-			
-			String[] cmd = {
-				"chmod 755 /usr/local/tomcat/bin/tomcatd"
-				,"chmod 755 /usr/local/tomcat/bin/catalina.sh"
-				,"cp /usr/local/tomcat/bin/tomcatd /etc/rc.d/init.d/tomcatd"
-				,"cd /usr/local/www && jar -xf sample.war"
-				,"cd /usr/local/www && rm -rf  META-INF sample.war"
-			};
-			js.exec(cmd);
-			
-		}else 	if("mysql".equals(server)){
-			 File file= new File(resource + "/config/" + server + "/my.cnf");
-			 js.scpTo(file, "/etc/my.cnf");
-		}else 	if("php".equals(server)){
-			 String[] frFile =	{resource + "/config/" + server + "/www.conf"
-									,resource + "/config/" + server + "/Info.php"};
-			String[] toFile =	{"/usr/local/php/etc/php-fpm.d/www.conf"
-									,"/usr/local/www/php/application/controllers/Info.php"};
-			copyRemote(frFile,toFile,js);
-		}else 	if("ftp".equals(server)){
-			
-		}
-		
-	}
-	
-	public void copyRemote(String[] frFile, String[] toFile, JSchUtil js){
-		for(int i = 0; i < frFile.length; i++){
-			File file =  new File(resource + frFile[i]);
-			 js.scpTo(file, toFile[i]);
-		}
-	}
-
-	public void copyRemote(String server, JSchUtil js){
+	public void copyResource(String server, JSchUtil js){
 		
 		String[] frFile = prop.getStringArray(server + ".frFile");
 		String[] toFile = prop.getStringArray(server + ".toFile");
